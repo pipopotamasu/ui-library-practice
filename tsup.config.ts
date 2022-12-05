098907e297e82190c565eee8476db3f8efaa6d80
pipopotamasu <ec0156hx@gmail.com>
2022-12-04 22:37:57 -0800

    add minify option

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,8 +3,9 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   entry: ['src/index.ts'],
   splitting: false,
+  minify: false,
   sourcemap: true,
   clean: true,
   treeshake: true,
-  dts: true
+  dts: true,
 });
